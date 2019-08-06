073a44041b70a62799f9164dbf70d36d68ac5905
MiaoWoo <admin@yumc.pw>
2019-08-06 09:18:34 +0800

    Create & Init core-market-service Project...

---
+++ src/main/java/com/sixi/core/marketservice/CoreMarketServiceApplication.java
@@ -0,0 +1,17 @@
+package com.sixi.core.marketservice;
+
+import org.springframework.boot.SpringApplication;
+
+import com.sixi.micro.common.annotation.SixiMicroServiceApplication;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @author MiaoWoo
+ */
+@SixiMicroServiceApplication
+public class CoreMarketServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoreMarketServiceApplication.class, args);
+    }
+}
